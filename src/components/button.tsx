09b3fbba3c6091dfd6c4586d6cc5afd77cc0e22d
thethoomm <thomas.santos02209@gmail.com>
2026-05-17 21:39:06 -0300

    fix: retira o className nao utilizado

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,8 +1,8 @@
 import { ButtonHTMLAttributes } from "react"
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-export function Button({ className = '', children, ...props }: ButtonProps) {
+export function Button({ children, ...props }: ButtonProps) {
     return (
         <button className="flex items-center justify-center gap-2 rounded-md px-4 py-2 text-sm font-medium transition-colors bg-primary text-white hover:bg-primary/90" {...props}>
             {children}
